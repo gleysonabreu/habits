5ef62e8309db8910a416d292374da3568b2f72d6
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-13 11:45:03 -0300

    move useEffect to summary component

--- src/pages/dashboard/habit/[id].tsx
+++ src/pages/dashboard/habit/[id].tsx
@@ -1,43 +1,16 @@
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import { Layout } from '../../../components/Layout';
-import { LoadingPage } from '../../../components/LoadingPage';
 import { Summary } from '../../../components/Summary';
-import { api } from '../../../libs/axios';
-
-export type SummaryItem = {
-  id: string;
-  date: Date;
-  completed: number;
-  amount: number;
-};
 
 export default function SummaryCalendar() {
-  const [summary, setSummary] = useState<SummaryItem[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
   const router = useRouter();
   const id = router.query.id as string;
 
-  useEffect(() => {
-    if (id) {
-      const loadingSummary = () => {
-        setLoading(true);
-        api.get(`/summary/${id}`).then(res => {
-          setSummary(res.data.summary);
-          setLoading(false);
-        });
-      };
-
-      loadingSummary();
-    }
-  }, [id]);
-
   return (
     <Layout>
-      {loading ? <LoadingPage /> : <Summary id={id} data={summary} />}
+      <Summary id={id} />
     </Layout>
   );
 }
