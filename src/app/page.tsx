e3f8f02648a663464f2d2e95bde891b716f58a16
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-05-16 11:45:02 -0300

    chore(home): add footer to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]/route';
 import Link from 'next/link'
 import { redirect } from 'next/navigation';
+import { Footer } from '@/components/Footer';
 
 export const metadata = { title: 'Hábitos' };
 
@@ -21,14 +22,18 @@ export default async function Home({
   const errorSigin = searchParams['error'] as SignInErrorTypes | undefined;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center md:flex-row md:justify-between p-7 md:p-24 gap-20 md:gap-5">
-      <div className='w-full flex items-center justify-center'>
-        <Link href='/' className='hover:opacity-80 scale-75 md:scale-100'>
-          <Logo />
-        </Link>
+    <main className="flex min-h-screen flex-col justify-between">
+      <div className='flex flex-col md:flex-row items-center flex-1 justify-center md:justify-between p-7 md:p-24 md:gap-5'>
+        <div className='w-full flex items-center justify-center'>
+          <Link href='/' className='hover:opacity-80 scale-75 md:scale-100'>
+            <Logo />
+          </Link>
+        </div>
+
+        <SignIn error={errorSigin} />
       </div>
 
-      <SignIn error={errorSigin} />
+      <Footer />
     </main>
   )
 }
