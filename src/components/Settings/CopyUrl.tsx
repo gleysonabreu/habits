49b38b7201d55a4d03c274fb00c8f1185deb6b9f
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-05-24 10:36:18 -0300

    chore: add check if link profile is copied

--- src/components/Settings/CopyUrl.tsx
+++ src/components/Settings/CopyUrl.tsx
@@ -1,19 +1,33 @@
 'use client';
 
-import { Copy } from "@phosphor-icons/react";
+import { Check, Copy } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
 
 type CopyUrlProps = {
   url: string;
 }
 
 export function CopyUrl({ url }: CopyUrlProps) {
+  const [isCopied, setIsCopied] = useState(false);
+
   function handleCopyUrl() {
+    setIsCopied(true);
     navigator.clipboard.writeText(url);
   }
 
+  useEffect(() => {
+    const timeCopied = setTimeout(() => setIsCopied(false), 5000);
+
+    return () => clearTimeout(timeCopied);
+  }, [isCopied]);
+
   return (
     <button title='Copiar?' onClick={handleCopyUrl} type='button' className="text-brand-primary hover:text-brand-secondary transition-all">
-      <Copy size={25} />
+      {isCopied ? (
+        <Check size={25} />
+      ) : (
+        <Copy size={25} />
+      )}
     </button>
   );
 }
