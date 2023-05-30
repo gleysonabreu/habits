ee1b617d75676b05eed54a58c25a77dfd5b3a261
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-05-30 12:46:36 -0300

    :bug: fix: button colors summary

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -16,11 +16,11 @@ export function Popover({ completedPercentage, children, date }: PopoverProps) {
     <PopoverRadix.Root>
       <PopoverRadix.Trigger title={dateFormatted} className={clsx('h-10 w-10 lg:h-5 lg:w-5 rounded-xl lg:rounded-md border-2 transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-background dark:focus:ring-offset-zinc-900 focus:ring-offset-zinc-100 focus:ring-sky-600', {
         'bg-zinc-300 border-zinc-400 dark:bg-zinc-800 dark:border-zinc-900': completedPercentage === 0,
-        'bg-blue-900 border-blue-500': completedPercentage > 0 && completedPercentage < 20,
-        'bg-blue-800 border-blue-500': completedPercentage >= 20 && completedPercentage < 40,
+        'bg-blue-500 border-blue-400': completedPercentage > 0 && completedPercentage < 20,
+        'bg-blue-600 border-blue-500': completedPercentage >= 20 && completedPercentage < 40,
         'bg-blue-700 border-blue-500': completedPercentage >= 40 && completedPercentage < 60,
-        'bg-blue-600 border-blue-500': completedPercentage >= 60 && completedPercentage < 80,
-        'bg-blue-500 border-blue-400': completedPercentage >= 80
+        'bg-blue-800 border-blue-500': completedPercentage >= 60 && completedPercentage < 80,
+        'bg-blue-900 border-blue-500': completedPercentage >= 80
       })
       } />
 
